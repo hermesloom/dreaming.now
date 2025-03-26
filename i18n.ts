a433d96f9e975dd8289849b29ccf44123ca6bda3
henophilia <tech@henophilia.org>
2025-03-26 09:55:03 +0000

    chore: remove everything

--- i18n.ts
+++
@@ -1,8 +0,0 @@
-import { getRequestConfig } from "next-intl/server";
-
-export default getRequestConfig(async ({ requestLocale }) => {
-  return {
-    messages: (await import(`./i18n/messages/${await requestLocale}.json`))
-      .default,
-  };
-});
