7c86046187e7d25a956112d33be03d4220533d81
henophilia <tech@henophilia.org>
2025-03-26 16:49:21 +0000

    fix: unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import MainContent from "@/components/MainContent";
