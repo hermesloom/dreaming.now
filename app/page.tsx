5df552e21fce7421a47ffd55dae7610134be8baa
henophilia <tech@henophilia.org>
2025-03-26 16:48:10 +0000

    fix: remove AddProjectDialog

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,12 @@
 import { useState } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import AddProjectDialog from "@/components/AddProjectDialog";
 import MainContent from "@/components/MainContent";
 
 export default function Home() {
-  const [dialogOpen, setDialogOpen] = useState(false);
-
   return (
     <div className="flex flex-col min-h-screen">
-      <Header onAddProject={() => setDialogOpen(true)} />
-
-      <AddProjectDialog open={dialogOpen} onOpenChange={setDialogOpen} />
+      <Header />
 
       <MainContent />
 
