4089a087a1f9a9ced9153373d1a1afc208aab26c
henophilia <tech@henophilia.org>
2025-04-09 11:35:28 +0100

    feat: add links/pb-atlas

---
+++ app/links/pb-atlas/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from "next/navigation";
+
+export default function PBAtlasRedirect() {
+  redirect(
+    "https://drive.google.com/file/d/14HADtrmNK18DnWrjEuHdYpUdcKHW4Wpo/view"
+  );
+}
